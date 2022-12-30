5ee5bc00e84a87cc7721ddaa49af844541fbf48d
Iñaki Cruz Elizondo Rios <116087814+CruzElizorios@users.noreply.github.com>
2022-12-29 21:33:20 -0300

    correccion textos

--- main.js
+++ main.js
@@ -27,40 +27,40 @@ function costoSegunCuota(cantidadCuotas) {
     switch (cantidadCuotas) {
         case 1:
             cuota = Math.round(credito * 1.1);
-            return alert("tendra que pagar " + cantidadCuotas + " cuota de " + cuota + " que contienen un 10% de interes a partir del proximo mes");
+            return alert("Tendrá que pagar " + cantidadCuotas + " cuota de $" + cuota + " que contienen un 10% de interés a partir del próximo mes");
         case 3:
             cuota = Math.round(credito/3);
             cuota = Math.round(cuota*1.3);
-            return alert("tendra que pagar "+ cantidadCuotas + " cuotas de " + cuota + " que contienen un 30% de interes a partir del proximo mes");
+            return alert("Tendrá que pagar "+ cantidadCuotas + " cuotas de $" + cuota + " que contienen un 30% de interés a partir del próximo mes");
         case 6:
             cuota = Math.round(credito/6);
             cuota = Math.round(cuota*1.45);
-            return alert("tendra que pagar "+ cantidadCuotas + " cuotas de " + cuota + " que contienen un 45% de interes a partir del proximo mes");
+            return alert("Tendrá que pagar "+ cantidadCuotas + " cuotas de $" + cuota + " que contienen un 45% de interés a partir del próximo mes");
         case 12:
             cuota = Math.round(credito/12);
             cuota = Math.round(cuota*1.60);
-            return alert("tendra que pagar "+ cantidadCuotas + " cuotas de " + cuota + " que contienen un 60% de interes a partir del proximo mes");
+            return alert("Tendrá que pagar "+ cantidadCuotas + " cuotas de $" + cuota + " que contienen un 60% de interés a partir del próximo mes");
         default:
-            alert("numero de cuotas no disponible, volver a elegir");
+            alert("Número de cuotas no disponible, volver a elegir");
             return cuotaElegida();
     }
 }
 
 function cuotaElegida(){
-    costoSegunCuota(Number(prompt("puede pagarlo en 1, 3, 6 o 12 cuotas, en cuantas desea pagarlo?")));
+    costoSegunCuota(Number(prompt("Puede pagarlo en 1, 3, 6 o 12 cuotas, ¿en cuántas desea pagarlo?")));
 }
 
 
-let solicitud = prompt("¿Quiere solicitar un credito? Si o No")
+let solicitud = prompt("¿Quiere solicitar un crédito? Si o No")
 if (solicitud === "Si" || solicitud === "si" || solicitud === "SI") {
     alert("Le podemos otorgar " + creditoDisponible(Number(prompt("¿Cuánto es su salario?"))));
     let x = "no"
     while (x === "no" || x === "No" || x === "NO" || x === "volver a elegir cuotas" || x === "volver") {
         cuotaElegida();
-        x = prompt("¿Quiere tomar el prestamo? Si / No (volver a elegir cuotas)")
+        x = prompt("¿Quiere tomar el préstamo? Si / No (volver a elegir cuotas)")
     }
-    alert("Felicitaciones, la solicitud de su credito ha sido exitosa")
-    alert("En unos momentos recibira el dinero ;)")
+    alert("Felicitaciones, la solicitud de su crédito ha sido exitosa")
+    alert("En unos momentos recibirá el dinero ;)")
 } else {
-    alert("lo esperamos la proxima")
+    alert("lo esperamos la próxima")
 }; 
